620579ccc2e35e67cdeb4520ea822c1985e7ef57
Sasha Demyanik <ttsvan@msn.com>
2019-01-16 13:31:38 -0800

    Initial commit

---
+++ src/containers/WeatherContent/WeatherContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class WeatherContent extends Component {
+    state = {  }
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default WeatherContent;
