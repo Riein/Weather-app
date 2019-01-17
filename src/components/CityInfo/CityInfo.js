7651ebe73b323753f4a9db0ac37d9f79b9f6a2c2
Sasha Demyanik <ttsvan@msn.com>
2019-01-16 18:33:04 -0800

    Allows user to input new zipcode

--- src/components/CityInfo/CityInfo.js
+++ src/components/CityInfo/CityInfo.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './CityInfo.module.css';
 
-const CityInfo = (props) => {
-    let iconURL = 'https://icons.wxug.com/i/c/j/' + props.icon + '.gif';
-    return (
-    <div className={classes.CityInfo}>
-        <h1>Weather in {props.city}</h1>
-        <p>City: {props.city}</p>
-        <p>State: {props.state}</p>
-        <p>Zip: {props.zip}</p>
-        <p>Temperature (fahrenheit): {props.tempf}</p>
-        <p>Temperature (celsius): {props.tempc}</p>
-        <p>Weather: {props.weather}</p>
-        <img src={iconURL} alt={props.icon} />
-    </div>
-    )
+class CityInfo extends Component {
+
+    state = {
+        input: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({ input: e.target.value });
+    }
+
+    render() {
+    let iconURL = 'https://icons.wxug.com/i/c/j/' + this.props.icon + '.gif';
+        return (
+        <div className={classes.CityInfo}>
+            <h1>Weather in {this.props.city}</h1>
+            <p>City: {this.props.city}</p>
+            <p>State: {this.props.state}</p>
+            <p>Zip: {this.props.zip}</p>
+            <p>Temperature (fahrenheit): {this.props.tempf}</p>
+            <p>Temperature (celsius): {this.props.tempc}</p>
+            <p>Weather: {this.props.weather}</p>
+            <img src={iconURL} alt={this.props.icon} />
+            <p> </p>
+            <input type="text" defaultValue={this.props.zip} onChange={this.handleChange}/>
+            <button onClick={() => {
+                (this.state.input.length === 5) ?
+                this.props.handleCity(this.state.input, this.props.zip) : alert("Input has incorrect length");
+            }
+            }>Search</button>
+        </div>
+        )
+    }
 };
 
 export default CityInfo;
--- src/containers/WeatherContent/WeatherContent.js
+++ src/containers/WeatherContent/WeatherContent.js
@@ -2,34 +2,33 @@ import React, {Component} from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import CityInfo from '../../components/CityInfo/CityInfo';
+import classes from './WeatherContent.module.css';
 
 class WeatherContent extends Component {
     state = { 
-        cities: {
-            city_info_1: {
-                zipcode: 90210,
-                city: '',
-                state: '',
-                temp_f: '',
-                temp_c: '',
-                weather: '',
-                icon: ''
-            },
-            city_info_2: {
-                zipcode: 60605,
-                city: '',
-                state: '',
-                temp_f: '',
-                temp_c: '',
-                weather: '',
-                icon: ''
-            }
+        city_info_1: {
+            zipcode: 90210,
+            city: '',
+            state: '',
+            temp_f: '',
+            temp_c: '',
+            weather: '',
+            icon: ''
+        },
+        city_info_2: {
+            zipcode: 60605,
+            city: '',
+            state: '',
+            temp_f: '',
+            temp_c: '',
+            weather: '',
+            icon: ''
         }
      }
 
    async componentDidMount() {
-        let zipcode1 = this.state.cities.city_info_1.zipcode;
-        let zipcode2 = this.state.cities.city_info_2.zipcode;
+        let zipcode1 = this.state.city_info_1.zipcode;
+        let zipcode2 = this.state.city_info_2.zipcode;
 
         const dataRes1 = await fetch("http://api.wunderground.com/api/" + process.env.REACT_APP_WEATHER_API_KEY +"/geolookup/conditions/q/"+ zipcode1 +".json");
         const data1 = await dataRes1.json();
@@ -37,7 +36,7 @@ class WeatherContent extends Component {
         const dataRes2 = await fetch("http://api.wunderground.com/api/" + process.env.REACT_APP_WEATHER_API_KEY +"/geolookup/conditions/q/"+ zipcode2 +".json");
         const data2 = await dataRes2.json();
 
-        this.setState({cities: {
+        this.setState({
             city_info_1: {
                 city: data1['location']['city'],
                 state: data1['location']['state'],
@@ -56,30 +55,58 @@ class WeatherContent extends Component {
                 weather: data2['current_observation']['weather'],
                 icon: data2['current_observation']['icon']
             },
-        }});
+        });
+    }
+
+    handleNewCity = (newZip, oldZip) => {
+        let c = (oldZip === this.state.city_info_1.zipcode) ? 'city_info_1' : 'city_info_2';
+        
+        fetch("http://api.wunderground.com/api/" + process.env.REACT_APP_WEATHER_API_KEY +"/geolookup/conditions/q/"+ newZip +".json")
+        .then((response) => response.json())
+        .then((data) => {
+
+            let newCity = {
+                city: data['location']['city'],
+                state: data['location']['state'],
+                zipcode: data['location']['zip'],
+                temp_f: data['current_observation']['temp_f'],
+                temp_c: data['current_observation']['temp_c'],
+                weather: data['current_observation']['weather'],
+                icon: data['current_observation']['icon']
+            }
+            let obj = {};
+            obj[c] = newCity;
+
+            this.setState(obj);
+        }).catch((err) => alert("Request failed: Please input a correct zipcode!"));
+
     }
 
     render() {
         return (
-            <Aux>
+            <Aux className={classes.WeatherContent}>
+                <div className={classes.Container}>
                 <CityInfo 
-                    city={this.state.cities.city_info_1.city}
-                    state={this.state.cities.city_info_1.state}
-                    zip={this.state.cities.city_info_1.zipcode}
-                    tempf={this.state.cities.city_info_1.temp_f}
-                    tempc={this.state.cities.city_info_1.temp_c}
-                    weather={this.state.cities.city_info_1.weather}
-                    icon={this.state.cities.city_info_1.icon}
+                    city={this.state.city_info_1.city}
+                    state={this.state.city_info_1.state}
+                    zip={this.state.city_info_1.zipcode}
+                    tempf={this.state.city_info_1.temp_f}
+                    tempc={this.state.city_info_1.temp_c}
+                    weather={this.state.city_info_1.weather}
+                    icon={this.state.city_info_1.icon}
+                    handleCity={this.handleNewCity}
                 />
                 <CityInfo 
-                    city={this.state.cities.city_info_2.city}
-                    state={this.state.cities.city_info_2.state}
-                    zip={this.state.cities.city_info_2.zipcode}
-                    tempf={this.state.cities.city_info_2.temp_f}
-                    tempc={this.state.cities.city_info_2.temp_c}
-                    weather={this.state.cities.city_info_2.weather}
-                    icon={this.state.cities.city_info_2.icon}
+                    city={this.state.city_info_2.city}
+                    state={this.state.city_info_2.state}
+                    zip={this.state.city_info_2.zipcode}
+                    tempf={this.state.city_info_2.temp_f}
+                    tempc={this.state.city_info_2.temp_c}
+                    weather={this.state.city_info_2.weather}
+                    icon={this.state.city_info_2.icon}
+                    handleCity={this.handleNewCity}
                 />
+                </div>
             </Aux>
         );
     }
